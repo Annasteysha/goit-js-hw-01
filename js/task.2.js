81b6d3b0d8f76aecc80d7bbfdd95b6fa322302e5
Annasteysha <154023562+Annasteysha@users.noreply.github.com>
2024-03-16 18:02:02 +0100

    finish-js-01.js

--- js/task.2.js
+++ js/task.2.js
@@ -5,3 +5,5 @@ console.log(`Shipping to ${country} will cost ${price + deliveryFee} credits`);
 getShippingMessage("Australia", 120, 50);
 getShippingMessage("Germany", 80, 20);
 getShippingMessage("Sweden", 100, 20);
+
+
--- js/task.3.js
+++ js/task.3.js
@@ -1,4 +1,5 @@
-function getElementWidth(content, padding, border) {
+
+ function getElementWidth(content, padding, border) {
     const contentWidth = parseFloat(content);
     const paddingWidth = parseFloat(padding);
     const borderWidth = parseFloat(border);
@@ -6,7 +7,6 @@ function getElementWidth(content, padding, border) {
     return contentWidth + paddingWidth * 2 + borderWidth * 2;
 }
 
-
-getElementWidth("50px", "8px", "4px");
-getElementWidth("60px", "12px", "8.5px");
-getElementWidth("200px", "0px", "0px");
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
